49a4bb3fedcbd64ddf616b42265334af5838e6ce
Fedor Baart <fedor.baart@deltares.nl>
2016-08-12 13:29:55 +0200

    move models to component

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,15 @@
   new Vue({
     el: '#app',
     data: {
+    },
+    events: {
+      'model-selected': function(model) {
+        console.log('app detected model selected', model);
+      }
     }
   })
   Vue.config.debug = true;
+
+
 
 }());
--- app/scripts/models.js
+++ app/scripts/models.js
@@ -1,39 +1,51 @@
-'use strict';
+(function () {
 
-$(function(){
-  // get models.json
-  fetch('data/models.json')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(json) {
-      var source = $('#model-template').html();
-      var template = Handlebars.compile(source);
-      var html = template(json);
-      $('#models').html(html);
-      _.map(json.models, function(model){
-        // find the model and add a click event to the cog link
-        $('#' + model.id).find('a.load-model').click(
-          function(){
-            console.log(model);
-            var event = new CustomEvent(
+  'use strict';
+
+  Vue.component('model-overview', {
+    template: '#model-overview-template',
+    data: function() {
+      return {
+        models: []
+      };
+    },
+    ready: function() {
+      // get models.json
+      fetch('data/models.json')
+        .then((response) => {
+          return response.json();
+        })
+        .then((json) => {
+          console.log('this', this);
+          this.models = json.models;
+          // select first model
+          document.dispatchEvent(
+            new CustomEvent(
               'model-selected',
-              {'detail': model}
-            );
-            document.dispatchEvent(event);
-          }
-        );
-      });
-      // select first model
-      document.dispatchEvent(
-        new CustomEvent(
-          'model-selected',
-          {'detail': json.models[0]}
-        )
-      );
-    })
-    .catch(function(ex) {
-      console.log('parsing failed', ex);
-    });
+              {'detail': json.models[0]}
+            )
+          );
+        })
+        .catch(function(ex) {
+          console.log('parsing failed', ex);
+        });
 
-});
+    },
+    methods: {
+      selectModel: function(model) {
+        // find the model and add a click event to the cog link
+        console.log('model selected', model);
+        var event = new CustomEvent(
+          'model-selected',
+          {'detail': model}
+        );
+        document.dispatchEvent(event);
+        // dispatch to parent
+        this.$dispatch('model-selected', model);
+
+      }
+    }
+
+  });
+
+}());
