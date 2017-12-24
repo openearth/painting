9e98d07bd9b71dfad37ce09634964b7c8d3d4a01
Fedor Baart <fedor.baart@deltares.nl>
2017-12-24 15:35:49 +0100

    seperate js file

---
+++ src/components/v-main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+
+import Vue2MapboxGL from 'vue2mapbox-gl';
+Vue.use(Vue2MapboxGL);
+export default {
+  name: '',
+  data () {
+    return {
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+  computed: {
+  }
+
+}
